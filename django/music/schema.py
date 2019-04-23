6b539226b00bed421bd8667eefdcc13d90ddaf4f
Aiden Huang <ahuang@mintel.com>
2019-04-23 13:37:41 +0800

    enable single object auery

--- django/music/schema.py
+++ django/music/schema.py
@@ -15,11 +15,25 @@ class AlbumType(DjangoObjectType):
 
 
 class Query(object):
+    artist = graphene.Field(ArtistType, id=graphene.Int())
+    album = graphene.Field(AlbumType, id=graphene.Int())
     all_artists = graphene.List(ArtistType)
     all_albums = graphene.List(AlbumType)
+
+    def resolve_artist(self, info, **kwargs):
+        id = kwargs.get('id')
+        if id:
+            return Artist.objects.get(pk=id)
+        return None
+
+    def resolve_album(self, info, **kwargs):
+        id = kwargs.get('id')
+        if id:
+            return Album.objects.get(pk=id)
+        return None
 
     def resolve_all_artists(self, info, **kwargs):
         return Artist.objects.all()
 
     def resolve_all_albums(self, info, **kwargs):
-        return Album.objects.select_related('artist').all()
+        return Album.objects.all()
